a1ff9b7a11c90ef49982cc46ba6134182f29479f
Mehmet <mhmtsytrk@gmail.com>
2020-10-01 20:32:16 +0300

    AutomationClass

--- src/test/java/com/cybertek/tests/day14_extent_reports/ExtentDemoTest.java
+++ src/test/java/com/cybertek/tests/day14_extent_reports/ExtentDemoTest.java
@@ -5,6 +5,7 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.cybertek.utilities.ConfigurationReader;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -61,6 +62,13 @@ public class ExtentDemoTest {
 
         //pass()--> marks the test case as passed
         extentLogger.pass("TC123 is passed");
+
+    }
+
+    @AfterMethod
+    public void teardown(){
+        //this is when the report is actually created
+        report.flush();
 
     }
 
